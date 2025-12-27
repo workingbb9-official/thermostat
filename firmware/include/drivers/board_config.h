847b3e19cc425b67f5e8fd1ca3b3218c2cda5750
AC <workingbb9@gmail.com>
2025-12-26 19:06:27 -0700

    refactor(fw/drivers): define therm pin in board config

---
+++ firmware/include/drivers/board_config.h
@@ -0,0 +1,6 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define THERM_PIN 0
+
+#endif // BOARD_CONFIG_H
--- firmware/src/drivers/thermistor.c
+++ firmware/src/drivers/thermistor.c
@@ -2,6 +2,8 @@
 
 #include <avr/io.h>
 
+#include "board_config.h"
+
 void therm_init(void) {
 	ADMUX |= (1 << REFS0);	// Use 5v as reference voltage
 	ADMUX &= ~(1 << ADLAR); // Use right-adjusted result
@@ -11,7 +13,7 @@ void therm_init(void) {
 }
 
 uint16_t therm_read(void) {
-	ADMUX = (ADMUX & 0xF0) | (0 & 0x0F); // Clear lower 4 bits
+	ADMUX = (ADMUX & 0xF0) | (THERM_PIN & 0x0F); // Clear lower 4 bits
 	ADCSRA |= (1 << ADSC); // Start conversion
 
 	while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
