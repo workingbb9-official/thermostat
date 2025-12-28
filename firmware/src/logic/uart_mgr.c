619a464d9eb29e16d8855085ae1b9357b7aedc67
AC <workingbb9@gmail.com>
2025-12-27 18:42:22 -0700

    feat(fw/uart): add function to send data packet struct

--- firmware/src/logic/uart_mgr.c
+++ firmware/src/logic/uart_mgr.c
@@ -14,3 +14,15 @@ void uart_mgr_transmit_string(const char *data) {
 
     uart_transmit_byte('\0');
 }
+
+void uart_mgr_transmit(const DataPacket *packet) {
+    uart_transmit_byte(packet->start_byte);
+    uart_transmit_byte(packet->type);
+    uart_transmit_byte(packet->length);
+
+    for (uint8_t i = 0; i < packet->length; ++i) {
+        uart_transmit_byte(packet->payload[i]);
+    }
+
+    uart_transmit_byte(packet->checksum);
+}
