86aed5a5183fd301353268b71e2b5c53886e7eef
AC <workingbb9@gmail.com>
2025-12-30 19:08:07 -0700

    refactor(firmware/drivers): review drivers
    Remove redundant comments.
    Cleanup some doxygen comments.

--- firmware/src/drivers/uart.c
+++ firmware/src/drivers/uart.c
@@ -12,15 +12,13 @@ void uart_init(void) {
     UBRR0L = UBRRL_VALUE;
 
     #if USE_2X
-        // UCSR0A |= (1 << U2X0);
-        SET_BIT(UCSR0A, U2X0);
+        SET_BIT(UCSR0A, U2X0); // Double speed mode if required
     #endif
 
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);   // Enable transmit and receive
 
     // Set data settings (1 stop bit, no parity, 8 bits)
     UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
-    // UCSR0B &= ~(1 << UCSZ02);
     CLR_BIT(UCSR0B, UCSZ02);
 }
 
