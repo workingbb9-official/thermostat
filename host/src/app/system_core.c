c0e645006f96f7a3207e1e8b9b6679fc517168fa
AC <workingbb9@gmail.com>
2025-12-31 23:00:27 -0700

    feat(host/app): add dynamic data array

--- host/src/app/system_core.c
+++ host/src/app/system_core.c
@@ -33,10 +33,13 @@ void system_init(void) {
 void system_run(void) {
     DataPacket packet = {0};
     const int receive_status = system_receive_data(&packet);
+
     if (receive_status == -1) {
         printf("Invalid packet\n");
+        return;
     } else if (receive_status == -2) {
         printf("Error receive data\n");
+        return;
     }
 
     switch (packet.type) {
@@ -63,33 +66,55 @@ void system_cleanup(void) {
 }
 
 void system_analyze(void) {
-    float data[256];
-    int line = 1;
+    size_t capacity = 256;
+    size_t count = 0;
+    float *data = malloc(capacity * sizeof(float));
 
-    while (storage_mgr_read_temp(&temp_line, line) != 0) {
-        if (line - 1 > 256) {
-            break;
+    if (data == NULL) {
+        printf("Error malloc\n");
+        system_cleanup();
+    }
+
+    float temp_line;
+    size_t line = 1;
+
+    while (storage_mgr_read_temp(&temp_line, line) == 0) {
+        if (count >= capacity) {
+            size_t new_capacity = capacity * 2;
+            float *temp_ptr = realloc(data, new_capacity * sizeof(float));
+
+            if (temp_ptr == NULL) {
+                printf("Error realloc\n");
+                free(data);
+                data = NULL;
+                system_cleanup();
+            }
+
+            capacity = new_capacity;
+            data = temp_ptr;
         }
 
-        float temp_line;
-
         printf("Data line: %.2f\n", temp_line);
-        data[line - 1] = temp_line;
+        data[count] = temp_line;
+
+        ++count;
         ++line;
     }
 
-    const float average = analyze_average(data, (size_t) line - 1);
-    const float max = analyze_max(data, (size_t) line - 1);
-    const float min = analyze_min(data, (size_t) line - 1);
+    const float average = analyze_average(data, (size_t) count);
+    const float max = analyze_max(data, (size_t) count);
+    const float min = analyze_min(data, (size_t) count);
 
     printf("Average: %f\n", average);
     printf("Max: %f\n", max);
     printf("Min: %f\n", min);
+
+    free(data);
+    data = NULL;
 }
 
-
 static void signal_init(void) {
-    struct sigaction sa;
+    struct sigaction sa = {0};
     sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
