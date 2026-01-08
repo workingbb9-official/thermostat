641ee9894ffdc932de5b49141babb92ef8701aab
AC <workingbb9@gmail.com>
2026-01-07 20:23:50 -0700

    refactor(host/port): remove system data receiver and use manager directly

--- host/src/app/system_core.c
+++ host/src/app/system_core.c
@@ -8,12 +8,12 @@
 #include "analysis/analysis.h"
 #include "port/port.h"
 #include "common/protocol.h"
-#include "app/system_data_receiver.h"
 
 static int signal_shutdown = 0;
-
 static ThermStatus signal_init(void);
 static void signal_handler(int signum);
+
+static int receive_data(DataPacket *packet);
 static void handle_temp(const DataPacket *packet);
 
 
@@ -32,7 +32,7 @@ ThermStatus system_init(void) {
 
 void system_run(void) {
     DataPacket packet = {0};
-    int receive_status = system_receive_data(&packet);
+    int receive_status = receive_data(&packet);
 
     if (receive_status == -1) {
         printf("Invalid packet\n");
@@ -143,4 +143,64 @@ static void handle_temp(const DataPacket *packet) {
     const int16_t value = (int16_t) (((uint16_t) packet->payload[0] << 8) | packet->payload[1]);
     const float data = value / 100.0f;
     storage_mgr_write_temp(data);
+}
+
+static int receive_data(DataPacket *packet) {
+    uint8_t first_byte;
+    if (port_mgr_read_byte(&first_byte) != 0) {
+        return -2;
+    }
+
+    if (first_byte != START_BYTE) {
+        return -1;
+    }
+    packet->start_byte = first_byte;
+
+    uint8_t second_byte;
+    if (port_mgr_read_byte(&second_byte) != 0) {
+        return -2;
+    }
+
+    switch (second_byte) {
+    case LOGIN:
+        packet->type = LOGIN;
+        break;
+    case TEMP:
+        packet->type = TEMP;
+        break;
+    case STATS:
+        packet->type = STATS;
+        break;
+    default:
+        return -1;
+        break;
+    }
+
+    uint8_t third_byte;
+    if (port_mgr_read_byte(&third_byte) != 0) {
+        return -2;
+    }
+
+    if (third_byte < 1 || third_byte > MAX_PAYLOAD) {
+        return -1;
+    }
+    packet->length = third_byte;
+    
+    for (uint8_t i = 0; i < packet->length; ++i) {
+        uint8_t payload_byte;
+        if (port_mgr_read_byte(&payload_byte) != 0) {
+            return -2;
+        }
+        
+        packet->payload[i] = payload_byte;
+    }
+    
+    uint8_t checksum_byte;
+    if (port_mgr_read_byte(&checksum_byte) != 0) {
+        return -2;
+    }
+
+    packet->checksum = checksum_byte;
+
+    return 0;
 }
