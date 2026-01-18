3b43cd7a2abd656fa15afe695bb65da88d093a63
AC <workingbb9@gmail.com>
2026-01-17 17:28:34 -0700

    fixup! refactor(fw/app): polish login state

--- firmware/src/app/state_login.c
+++ firmware/src/app/state_login.c
@@ -5,24 +5,31 @@
 #include <firmware/lcd.h>
 #include "states.h"
 
-#define PASSWORD_LEN 4
-const char password[PASSWORD_LEN + 1] PROGMEM = "1234";
+#define LOGIN_PWD_LEN 4
+const char pwd_actual[LOGIN_PWD_LEN + 1] PROGMEM = "1234";
 
-enum password_state {
-    PASS_VALID = 0,
-    PASS_INVALID
+enum pwd_state {
+    PWD_VALID = 0,
+    PWD_INVALID
 } __attribute__((packed));
 
 static struct {
-    char            user_input[PASSWORD_LEN + 1];
-    uint8_t         pos;
-    char            key;
     struct {
-        uint8_t     key_pending     : 1;
-        uint8_t     lcd_dirty       : 1;
-        uint8_t     auth_failed     : 1;
+        char buf[LOGIN_PWD_LEN + 1];
+        uint8_t idx;
+    } pwd;
+    char input;
+
+    union {
+        uint8_t all;
+        struct {
+            uint8_t lcd_dirty   : 1;
+            uint8_t input_pending : 1;
+            uint8_t auth_failed : 1;
+            uint8_t reserved    : 5;
+        }
     } flags;
-} login_data;
+} login_ctx;
 
 static void login_init(void);
 static void login_keypress(void);
@@ -30,22 +37,21 @@ static void login_process(void);
 static void login_display(void);
 
 const struct state_actions login_state = {
-    .init           = login_init,
-    .on_keypress    = login_keypress,
-    .process        = login_process,
-    .display        = login_display,
-    .send           = 0,
-    .receive        = 0,
-    .exit           = 0
+    .init        = login_init,
+    .on_keypress = login_keypress,
+    .process     = login_process,
+    .display     = login_display,
+    .send        = 0,
+    .receive     = 0,
+    .exit        = 0
 };
 
-static enum password_state validate_input(const char *user_input);
+static enum pwd_state pwd_validate(void);
 
 static void login_init(void) {
-    login_data.pos = 0;
-    login_data.flags.key_pending = 0;
-    login_data.flags.lcd_dirty = 1;
-    login_data.flags.auth_failed = 0;
+    login_ctx.flags.all = 0;
+    login_ctx.pwd.buf[0] = '\0';
+    login_ctx.pwd.idx = 0;
 }
 
 static void login_keypress(void) {
@@ -54,56 +60,63 @@ static void login_keypress(void) {
         keypad.current_key == keypad.last_key)
         return;
 
-    login_data.key = keypad.current_key;
-    login_data.flags.key_pending = 1;
+    login_ctx.flags.input_pending = 1;
+    login_ctx.input = keypad.current_key;
 }
 
 static void login_process(void) {
-    if (login_data.flags.key_pending) {
-        login_data.flags.key_pending = 0;
-        login_data.user_input[login_data.pos] = login_data.key;
-        login_data.user_input[login_data.pos + 1] = '\0';
-        ++login_data.pos;
-        login_data.flags.lcd_dirty = 1;
+    if (login_ctx.flags.input_pending) {
+
+        login_ctx.flags.input_pending = 0;
+
+        login_ctx.pwd.buf[login_ctx.pwd.idx] = login_ctx.input;
+        login_ctx.pwd.buf[login_ctx.pwd.idx + 1] = login_ctx.input;
+        ++login_ctx.pwd.idx;
+
+        login_ctx.flags.lcd_dirty = 1;
     }
 
-    if (login_data.pos == PASSWORD_LEN) {
-        if (validate_input(login_data.user_input) == PASS_VALID) {
-            sys_change_state(&home_state);
-        } else {
-            login_data.pos = 0;
-            login_data.flags.lcd_dirty = 1;
-            login_data.flags.auth_failed = 1;
-        }
+    if (login_ctx.pwd.idx != LOGIN_PWD_LEN)
+        return;
+
+    if (pwd_validate() == PWD_VALID) {
+        sys_change_state(&home_state);
+    } else {
+
+        login_ctx.pwd.idx = 0;
+
+        login_ctx.flags.lcd_dirty = 1;
+        login_ctx.flags.auth_failed = 1;
     }
 }
 
 static void login_display(void) {
-    if (!login_data.flags.lcd_dirty)
+    if (!login_ctx.flags.lcd_dirty)
         return;
 
-    login_data.flags.lcd_dirty = 0;
-    lcd_mgr_clear();
+    login_ctx.flags.lcd_dirty = 0;
 
-    if (login_data.flags.auth_failed) {
-        login_data.flags.auth_failed = 0;
+    lcd_mgr_clear();
+    if (login_ctx.flags.auth_failed) {
+
+        login_ctx.flags.auth_failed = 0;
+
         lcd_mgr_write("Invalid password");
         lcd_mgr_set_cursor(1, 0);
         lcd_mgr_write("3 more tries");
     } else {
         lcd_mgr_write("Enter password");
         lcd_mgr_set_cursor(1, 0);
-        lcd_mgr_write(login_data.user_input);
+        lcd_mgr_write(login_ctx.pwd.buf);
     }
 }
 
 
-static enum password_state validate_input(const char *user_input) {
-    for (uint8_t i = 0; i < PASSWORD_LEN; ++i) {
-        if (user_input[i] != password[i]) {
-            return PASS_INVALID;
-        }
+static enum pwd_state pwd_validate(void) {
+    for (uint8_t i = 0; i < LOGIN_PWD_LEN; ++i) {
+        if (login_ctx.pwd.buf[i] != pwd_actual[i])
+            return PWD_INVALID;
     }
 
-    return PASS_VALID;
+    return PWD_VALID;
 }
