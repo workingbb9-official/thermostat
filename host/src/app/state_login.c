a0adf37f1680c12e19c360b45e8b96b3a56d1d7f
AC <workingbb9@gmail.com>
2026-01-31 18:06:43 -0700

    feat(host/app): record firmware logins

--- host/src/app/state_login.c
+++ host/src/app/state_login.c
@@ -1,7 +1,34 @@
 #include "state_login.h"
 
-#include <host/common/tsys_errors.h>
+#include <stdio.h>
+#include <time.h>
 
-enum tsys_err login_record_login(void) {
+#include <host/common/tsys_errors.h>
+#include <host/file_utils.h>
+
+enum tsys_err login_record_login(int login_fd) {
+    const time_t time_abs = time(NULL);
+    struct tm * const time = localtime(&time_abs);
+    printf("Login at %d:%d:%d\n", time->tm_hour, time->tm_min, time->tm_sec);
+    
+    // Convert timestamp to string
+    char buf[32];
+    const int len = snprintf(
+            buf, sizeof(buf), "%d-%d-%dT%d:%d:%d",
+            time->tm_year, time->tm_mon, time->tm_mday, 
+            time->tm_hour, time->tm_min, time->tm_sec);
+
+    // Seek to end to setup writing
+    if (file_seek(login_fd, END) < 0) {
+        printf("Record login: failed to seek\n");
+        return TSYS_E_FILE;
+    }
+
+    // Write timestamp to login file
+    if (file_write_line(login_fd, buf, len) < 0) {
+        printf("Record login: failed to write line\n");
+        return TSYS_E_FILE;
+    }
+
     return TSYS_OK;
 }
